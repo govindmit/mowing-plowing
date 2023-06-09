628adfe9885921926329d194f09fa1529b406603
Govind Namdev <govind@mangoiotsolutions.com>
2023-06-09 19:20:00 +0530

    Init Git

---
+++ app/Models/Property.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'address',
+        'category_id',
+        'user_id',
+        'lat',
+        'lng',
+    ];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('m-d-Y H:i:s');
+    }
+
+    public function orders(){
+        return $this->hasMany(Order::class);
+    }
+
+    // public function ordersCount(){
+    //     return $this->hasMany(Order::class)->count();
+    // }
+}
